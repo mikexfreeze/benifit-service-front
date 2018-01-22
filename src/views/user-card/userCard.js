7248d493d747bc9456fbf1d4e795dc931f90fe64
Mike <mikexfreeze@gmail.com>
2018-01-22 15:58:40 +0800

    整理

--- src/views/user-card/userCard.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Micheal Xiao on 2018/1/22.
- */
--- src/views/user-card/userCard.vue
+++
@@ -1,6 +0,0 @@
-<template>
-
-</template>
-
-<script src="./userCard.js"></script>
-<style scoped lang="scss" src="./userCard.scss"></style>
