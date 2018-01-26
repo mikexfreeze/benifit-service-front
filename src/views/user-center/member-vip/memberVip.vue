75c82e6dc50f8db89c422409994e56f612570ce3
yancui <460246473@qq.com>
2018-01-26 14:21:57 +0800

    页面修改

--- src/views/user-center/member-vip/memberVip.vue
+++ src/views/user-center/member-vip/memberVip.vue
@@ -2,11 +2,12 @@
   <div class="Member">
       <div class="TopTEXT"></div>
       <div class="Tab">
-            <div class="active">粉卡会员</div>
-            <div>银卡会员</div>
-            <div>金卡会员</div>
+            <div :class="{active:''}" v-for="item in items" @click="TabContent">{{item.message}}</div>   
         </div>
-        <div class="content">
+        <div v-if="ok" class="content">
+         <p class="Ptext">粉卡会员12个月内消费金额累计达到1200元，
+              即可升级为银卡会员，等级有效期为一年，
+              累计消费1500元可延长会籍一年</p>
           <div class="content-part1">
             <div class="leftPic"></div>
             <div class="rightPic"></div>
@@ -23,12 +24,76 @@
             <div class="leftPic"></div>
             <div class="rightPic"></div>
           </div>
+          <div class="snow"></div>
+          <div class="Car"></div>
         </div>
-        <div class="snow"></div>
+         <div v-else-if="ok" class="content2">
+           <p class="Ptext">粉卡会员12个月内消费金额累计达到1200元，
+              即可升级为银卡会员，等级有效期为一年，
+              累计消费1500元可延长会籍一年</p>
+          <div class="content-part1">
+            <div class="leftPic"></div>
+            <div class="rightPic"></div>
+          </div>
+           <div class="content-part2 mt5">
+            <div class="leftPic"></div>
+            <div class="rightPic"></div>
+          </div>
+           <div class="content-part3 mt5">
+            <div class="leftPic"></div>
+            <div class="rightPic"></div>
+          </div>
+            <div class="content-part4 mt5">
+            <div class="leftPic"></div>
+            <div class="rightPic"></div>
+          </div>
+          <div class="snow"></div>
+          <div class="Car"></div>
+        </div>
+         <div v-else class="content3">
+           <p class="Ptext">粉卡会员12个月内消费金额累计达到1200元，
+              即可升级为银卡会员，等级有效期为一年，
+              累计消费1500元可延长会籍一年</p>
+          <div class="content-part1">
+            <div class="leftPic"></div>
+            <div class="rightPic"></div>
+          </div>
+           <div class="content-part2 mt5">
+            <div class="leftPic"></div>
+            <div class="rightPic"></div>
+          </div>
+           <div class="content-part3 mt5">
+            <div class="leftPic"></div>
+            <div class="rightPic"></div>
+          </div>
+            <div class="content-part4 mt5">
+            <div class="leftPic"></div>
+            <div class="rightPic"></div>
+          </div>
+          <div class="snow"></div>
         <div class="Car"></div>
+        </div>
+        <div class="explanation"><p>*上述所有会员积分及会员权益仅在贝玲妃上海来福士/北京三里屯/
+广州正佳/深圳海岸城/深圳万象城精品店适用</p></div>
   </div>
 </template>
 <script>
-    export default{}
+    export default{
+      data(){
+         return {
+           ok:true,
+           items: [
+            { message: '粉卡会员' },
+            { message: '银卡会员' },
+            { message: '金卡会员' },
+          ]
+         }
+      },
+      methods: {
+        TabContent() {
+          console.log(1)
+        }
+      },
+    }
 </script>
 <style scoped lang="scss" src="./memberVip.scss"></style>
